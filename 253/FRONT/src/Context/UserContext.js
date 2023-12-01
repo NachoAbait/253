fea2528995bc46f5d07541c1304720b512c967c0
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-12-01 11:34:16 -0300

    fd

--- 253/FRONT/src/Context/UserContext.js
+++ 253/FRONT/src/Context/UserContext.js
@@ -48,7 +48,7 @@ export const UserProvider = ({ children }) => {
       // Error en la creación del usuario
       if (error.message) {
         // Utiliza el mensaje de error lanzado por la acción login
-        alert(error.message.data);
+        alert(error);
       } else {
         // Mensaje de error genérico o específico
         console.error(error);
